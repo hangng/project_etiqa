a1c401e902b2d518b82e1c2ead88834fa1e502a7
Dino <chuanhang.ng@silverglobe.com>
2022-10-17 20:40:47 +0800

    fix - reflect list after add or delete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:maybank_etiqa/main.dart';
+import 'package:maybank_etiqa/main1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
